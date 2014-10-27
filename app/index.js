3c75721ec75f5a47071ecf795689c7018fadd422
Peter Arato <it.arato@gmail.com>
2014-10-27 20:10:40 +0000

    Add mkdir.

--- app/index.js
+++ app/index.js
@@ -8,17 +8,29 @@ var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
 
-  prompting: function () {
-    this.log('Prompting.');
-    //this.prompt('Hello');
-  },
-
   constructor: function () {
     generators.Base.apply(this, arguments);
   },
 
+  prompting: function () {
+    this.log('Prompting.');
+
+    this.prompt([{
+      type: 'input',
+      name: 'profileName',
+      message: 'Profile name'
+    }], function ( answers ) {
+      this.profileName = answers.profileName;
+    }.bind(this));
+
+    this.async();
+  },
+
   generateFolder: function () {
     this.log('Folder generation.');
+
+    // @todo to machine name
+    this.mkdir(this.profileName);
   }
 
 });
